414c87b8ee46313fb3fa8924a70d3d55059a0430
egiit <giefilt@gmail.com>
2025-02-12 07:42:58 +0700

    add controll be createupdate and func role

--- src/pages/UserBackendRole.js
+++ src/pages/UserBackendRole.js
@@ -5,27 +5,55 @@ import { AuthContext } from "../auth/AuthProvider";
 import axios from "../axios/axios";
 import { toast } from "react-toastify";
 import DropdownCus from "../partial/DropdownCus";
+import MenutAuth from "../component/compRegister/MenutAuth";
+import { Typeahead } from "react-bootstrap-typeahead";
+import "react-bootstrap-typeahead/css/Typeahead.css";
 
 const intialBeObj = {
   BE_ROLE_NAME: "",
   BE_STATUS: 1,
-  BE_ROLE_TYPE: "",
+  BE_ROLE_TYPE: 3,
   BE_PATH: "",
   BE_NOTE: "",
 };
 
 const UserBackendRole = () => {
-  const { value } = useContext(AuthContext);
+  const { value, mainState } = useContext(AuthContext);
   const { userId, idPerusahaan } = value;
   const [show, setShow] = useState(false);
   const [listBE, setListBe] = useState([]);
+  const [listTypeRole, setListTypeRole] = useState([]);
   const [method, setMethod] = useState("post");
+  const [beId, setBeId] = useState("post");
+  const [actType, setActionType] = useState("CREATE");
   const [activeDropdown, setActiveDropdown] = useState(null);
+  const [menus, setMenus] = useState([]);
+  const [tabMenu, settabMenu] = useState(false);
+  const [dataPerushaan, setDataPerusahaan] = useState([]);
+  const [idPerushaan, setIdPerusahaan] = useState("");
+  const [perusahaan, setPerushaan] = useState([]);
+  const [disabledPerusahaan, setDisabelPerusahaan] = useState(false);
+
+  //menu access perushaan
+  const [arrView, setarrView] = useState([]);
+  const [menuAcces, setMenuAcces] = useState([]);
 
   const [formData, setFormData] = useState(intialBeObj);
 
+  //function cari menu
+  const getMenus = async (idPerusahaan) => {
+    if (idPerusahaan) {
+      const response = await axios.get(`/menubynpwp/${idPerusahaan}`);
+      return setMenus(response.data);
+    }
+    const repsons = await axios.get("/menu");
+    return setMenus(repsons.data);
+  };
+
   function handleClose() {
     setShow(false);
+    setFormData(intialBeObj);
+    setActionType("Create");
   }
 
   function handleShow() {
@@ -35,6 +63,7 @@ const UserBackendRole = () => {
 
   function handleChange(e) {
     const { name, value } = e.target;
+
     setFormData((prevFormData) => {
       return {
         ...prevFormData,
@@ -43,16 +72,30 @@ const UserBackendRole = () => {
     });
   }
 
+  function handleStatus(value) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        BE_STATUS: value,
+      };
+    });
+  }
+
   async function handleSubmit(e) {
     e.preventDefault();
-    const dataPost = { ...formData, BE_ADD_ID: userId };
-    // console.log(dataPost);
+    const dataPost = {
+      ...formData,
+      BE_ID_PERUSAHAAN: idPerushaan,
+      BE_ADD_ID: userId,
+    };
 
     await axios[method](`/backend-role`, dataPost)
       .then((res) => {
         if (res.status === 200) {
           toast.success(res.data.message, { autoClose: 3000 });
+          getListBe(idPerusahaan);
           handleClose();
+          setFormData(intialBeObj);
         }
       })
       .catch((err) => toast.error(err.data.message, { autoClose: 3000 }));
@@ -62,7 +105,6 @@ const UserBackendRole = () => {
     setMethod(chgMethod);
   }
 
-  //for getlist backend role
   async function getListBe(idPerusahaan) {
     let url = `/backend-role`;
 
@@ -80,25 +122,161 @@ const UserBackendRole = () => {
       .catch((err) => toast.error(err.data.message, { autoClose: 3000 }));
   }
 
-  useEffect(() => {
-    getListBe(idPerusahaan);
-  }, [idPerusahaan]);
+  //for getlist type role
+  async function getLisTypeRole(userLevel) {
+    let url = `/backend-role/type-role`;
 
+    await axios
+      .get(url)
+      .then((res) => {
+        if (res.status === 200) {
+          // const filterType = res.data.data.filter(item => item.TYPE_ROLE_ID === 3);
+          setListTypeRole(res.data.data);
+        }
+      })
+      .catch((err) => toast.error(err.data.message, { autoClose: 3000 }));
+  }
+
+  const resetMenuArr = () => {
+    setBeId("");
+    setarrView([]);
+  };
+
+  const getMenuAccess = async (id, idPerusahaan) => {
+    // loadingOn();
+
+    let url = `/backend-role/detail-role/${id}/${idPerusahaan}`
+
+    const respons = await axios.get(url);
+    const arrViewd = [];
+    respons.data.data.forEach((ma) => {
+      arrViewd.push(ma.USER_ACESS_VIEW === 1 ? true : false);
+    });
+    
+    setarrView(arrViewd);
+    setMenuAcces(respons.data.data);
+    settabMenu(true);
+    setIdPerusahaan(idPerusahaan);
+  };
+
+  //model open funct role \ menu auth
   function openMdlMenu(idBe) {
-    console.log(idBe);
+    resetMenuArr();
+    setBeId(idBe);
+    const findBe = listBE.filter((item) => item.BE_ID === idBe);
+    
+    return getMenuAccess(idBe, findBe[0].BE_ID_PERUSAHAAN);
   }
 
   function editeBe(idBe) {
-    console.log(idBe);
+    const findBe = listBE.filter((item) => item.BE_ID === idBe);
+    setActionType("Edit");
+
+    if (findBe.length > 0) {
+      setFormData(findBe[0]);
+    }
+    setShow(true);
   }
 
   function actionList(id) {
     return [
       { actionLable: "Edit", actExe: () => editeBe(id) },
-      { actionLable: "Fucntion Role", actExe: () => openMdlMenu(id) },
+      {
+        actionLable: "Fucntion Role",
+        actExe: () => openMdlMenu(id),
+      },
     ];
   }
+  function handleMenuClose() {
+    setarrView([]);
 
+    setMenuAcces([]);
+    settabMenu(false);
+    setBeId("");
+  }
+
+  //fake function
+  function fake(a) {
+    return;
+  }
+
+  const saveResultbtn = async () => {
+    const arrNewAccs = [];
+    menuAcces.map(async (menu, index) => {
+      const dataAcces = {
+        ID_PERUSAHAAN: idPerushaan,
+        BE_ID: beId,
+        ACCESS_MENU_ID: arrView[index] ? menu.MENU_ID : null,
+        ADD_ID: userId,
+        MENU_NAME: menu.MENU_TITLE,
+      };
+      arrNewAccs.push(dataAcces);
+    });
+    
+    const filterAccKosong = arrNewAccs.filter(
+      (acc) => acc.ACCESS_MENU_ID !== null
+    );
+
+    await axios
+      .post(`/backend-role/detail-role/${beId}`, filterAccKosong)
+      .then((res) => {
+        if (res.status === 200) {
+          toast.success(res.data.message, { autoClose: 3000 });
+          setarrView([]);
+
+          setMenuAcces([]);
+          settabMenu(false);
+        } else {
+          toast.error(res.data.message, { autoClose: 3000 });
+        }
+      })
+      .catch((err) => {
+        toast.error("Something wrong call administrator", { autoClose: 3000 });
+      });
+  };
+
+  useEffect(() => {
+    async function getDataPerusahaan(id) {
+      let urlPerushaan =
+        id && mainState.userLevel !== "sa"
+          ? `/perusahaan/${id}`
+          : `/perusahaan`;
+      await axios
+        .get(urlPerushaan)
+        .then((res) => {
+          if (res.status === 200 && res.data.data?.length > 0) {
+            const listPerusahaan = res.data.data.map((per) => ({
+              ...per,
+              id: per.ID_PERUSAHAAN,
+              name: `${per.ID_PERUSAHAAN} - ${per.NAMA_PERUSAHAAN}`,
+            }));
+            setDataPerusahaan(listPerusahaan);
+
+            if (res.data.data?.length === 1) {
+              const { ID_PERUSAHAAN } = res.data.data[0];
+              setIdPerusahaan(ID_PERUSAHAAN);
+              setDisabelPerusahaan(mainState.userLevel !== "sa");
+            }
+          }
+        })
+        .catch((err) => console.log(err.data.message));
+    }
+
+    getMenus(idPerusahaan);
+    getDataPerusahaan(idPerusahaan);
+    getListBe(idPerusahaan);
+    getLisTypeRole(mainState.userLevel);
+  }, [idPerusahaan, mainState.userLevel]);
+
+  function handlePerusahaan(e) {
+    setPerushaan(e);
+    if (e.length > 0) {
+      const { ID_PERUSAHAAN } = e[0];
+      setIdPerusahaan(ID_PERUSAHAAN);
+    } else {
+      setIdPerusahaan("");
+    }
+  }
   return (
     <>
       <Row className="m-0 mt-2">
@@ -159,11 +337,27 @@ const UserBackendRole = () => {
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header className="border-0" closeButton>
-          <Modal.Title>Create</Modal.Title>
+          <Modal.Title>{actType}</Modal.Title>
         </Modal.Header>
         <Form onSubmit={handleSubmit}>
           <Modal.Body>
             <div className="mx-3">
+              <Form.Group as={Row} className="mb-3" controlId="forPerusahaan">
+                <Form.Label className="text text-end" size="sm" column sm={3}>
+                  Perusahaan
+                </Form.Label>
+                <Col sm={9}>
+                  <Typeahead
+                    clearButton
+                    id="perusahaan"
+                    labelKey="name"
+                    onChange={handlePerusahaan}
+                    options={dataPerushaan}
+                    selected={perusahaan}
+                    disabled={disabledPerusahaan}
+                  />
+                </Col>
+              </Form.Group>
               <Form.Group
                 as={Row}
                 className="mb-4"
@@ -178,6 +372,7 @@ const UserBackendRole = () => {
                     name="BE_ROLE_NAME"
                     onChange={handleChange}
                     value={formData.BE_ROLE_NAME}
+                    required
                   />
                 </Col>
               </Form.Group>
@@ -191,7 +386,9 @@ const UserBackendRole = () => {
                     type="radio"
                     label="Enable"
                     name="enable"
+                    checked={formData.BE_STATUS}
                     id="formEnable"
+                    onChange={() => handleStatus(1)}
                   />
                 </Col>
                 <Col className="align-content-center">
@@ -199,6 +396,8 @@ const UserBackendRole = () => {
                     type="radio"
                     label="Disabled"
                     name="disabled"
+                    checked={!formData.BE_STATUS}
+                    onChange={() => handleStatus(0)}
                     id="formDisabled"
                   />
                 </Col>
@@ -209,7 +408,18 @@ const UserBackendRole = () => {
                   Role Type
                 </Form.Label>
                 <Col sm={9}>
-                  <Form.Control type="text" placeholder="" />
+                  <Form.Select
+                    aria-label="Default select role"
+                    name="BE_ROLE_TYPE"
+                    defaultValue={3}
+                    onChange={handleChange}
+                  >
+                    {listTypeRole?.map((item) => (
+                      <option key={item.TYPE_ROLE_ID} value={item.TYPE_ROLE_ID}>
+                        {item.TYPE_ROLE_NAME}
+                      </option>
+                    ))}
+                  </Form.Select>
                 </Col>
               </Form.Group>
 
@@ -223,7 +433,7 @@ const UserBackendRole = () => {
                     name="BE_PATH"
                     onChange={handleChange}
                     value={formData.BE_PATH}
-                  />{" "}
+                  />
                 </Col>
               </Form.Group>
 
@@ -258,6 +468,31 @@ const UserBackendRole = () => {
           </Modal.Footer>
         </Form>
       </Modal>
+
+      {tabMenu ? (
+        <Modal show={tabMenu} onHide={() => handleMenuClose()} size="lg">
+          <Modal.Body>
+            <MenutAuth
+              menus={menus}
+              setarrView={setarrView}
+              setarrCreate={fake}
+              setarrUpdate={fake}
+              setarrDelete={fake}
+              setarrPrint={fake}
+              arrView={arrView}
+              arrCreate={[]}
+              arrUpdate={[]}
+              arrDelete={[]}
+              arrPrint={[]}
+              menuAcces={menuAcces}
+              saveResultbtn={() => saveResultbtn(idPerushaan)}
+              btnFalse={() => handleMenuClose()}
+            />
+          </Modal.Body>
+        </Modal>
+      ) : (
+        ""
+      )}
     </>
   );
 };
