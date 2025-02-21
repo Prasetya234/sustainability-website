d220175a574f7ee7bf2595204403c288ec1c496e
Aan Kristanto Nugroho <43904651+aankristanto@users.noreply.github.com>
2025-02-21 09:29:43 +0700

    add table header payslip

--- src/pages/PortalPayslip.js
+++ src/pages/PortalPayslip.js
@@ -4,8 +4,6 @@ import { Row, Col, Button, Card, Table } from "react-bootstrap";
 import { FaPlus, FaFileImport, FaTrash } from "react-icons/fa";
 
 const PortalPayslip = () => {
-    
-
     return (
         <Row className="mx-0 mt-3">
             <Col sm={12} className="ps-3 p-2">
@@ -17,13 +15,46 @@ const PortalPayslip = () => {
                     </Card.Header>
                     <Card.Body className="text rounded shadow-sm">
                         <Table>
-                            <thead>
+                            <thead className="text-center">
                                 <tr>
-                                    <th>Year / Month</th>
-                                    <th>Employee ID</th>
-                                    <th>Employee Name</th>
-                                    <th>Salary</th>
-                                    
+                                    <th>Year</th>
+                                    <th>Month</th>
+                                    <th>ID</th>
+                                    <th>Name</th>
+                                    <th>Departemen</th>
+                                    <th>Company</th>
+                                    <th>Job Title</th>
+                                    <th>Status</th>
+                                    <th>Birthday</th>
+                                    <th>Onboarding Date</th>
+                                    <th>Resign Date</th>
+                                    <th>Basic Salary</th>
+                                    <th>Prorate Salary</th>
+                                    <th>Grading Allowance</th>
+                                    <th>Work Length Allowance</th>
+                                    <th>Job Title Allowance</th>
+                                    <th>Non-Fixed Allowance</th>
+                                    <th>Skill Allowance</th>
+                                    <th>Working Day Total</th>
+                                    <th>Working Hour Total</th>
+                                    <th>Total Hour OT 1</th>
+                                    <th>Total Hour OT 2</th>
+                                    <th>OT Holiday</th>
+                                    <th>OT Work Day 1</th>
+                                    <th>OT Work Day 2</th>
+                                    <th>OT Holiday</th>
+                                    <th>Attendance Premi</th>
+                                    <th>Eating Cost</th>
+                                    <th>Menstrual Allowance</th>
+                                    <th>Transport Allowance</th>
+                                    <th>Reward Target</th>
+                                    <th>Shift Allowance</th>
+                                    <th>Absentee</th>
+                                    <th>Gross Salary</th>
+                                    <th>Union</th>
+                                    <th>PPh / Tax</th>
+                                    <th>Jamsostek</th>
+                                    <th>Net Salary</th>
                                 </tr>
                             </thead>
                         </Table>
@@ -31,7 +62,6 @@ const PortalPayslip = () => {
                 </Card>
             </Col>
             <Col>
-
             </Col>
         </Row>
     )
