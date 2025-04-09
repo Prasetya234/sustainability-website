5dbdd2ab0e6b2dc0d2b40af8563b1dcf697e125e
Aan Kristanto Nugroho <43904651+aankristanto@users.noreply.github.com>
2025-04-09 08:25:54 +0700

    fixing import excel on emp

--- src/pages/EmpManagement.js
+++ src/pages/EmpManagement.js
@@ -218,9 +218,9 @@ const EmpManagement = () => {
                 const values = rawData.slice(1); // Rest as data
                 
                 // Function to check if a value is an Excel date
-                // const isExcelDate = (value) => {
-                //     return typeof value === "number" && value > 0 && value < 2958465; // Excel's valid date range
-                // };
+                const isExcelDate = (value) => {
+                    return typeof value === "number" && value > 0 && value < 2958465; // Excel's valid date range
+                };
                 
                 // Convert array into array of objects
                 const formattedData = values.map((row) => {
@@ -229,10 +229,10 @@ const EmpManagement = () => {
                         let value = row[index];
 
                         // Convert only if the column is 'BIRTHDAY' or 'ONBOARDING_DATE'
-                        // if (['BIRTHDAY', 'ONBOARDING_DATE'].includes(key) && isExcelDate(value)) {
-                        //     const excelEpoch = new Date(Date.UTC(1899, 11, 30)); // Excel starts at Dec 30, 1899
-                        //     value = moment.utc(excelEpoch.getTime() + value * 86400000).format('YYYY-MM-DD');
-                        // }
+                        if (['BIRTHDAY', 'ONBOARDING_DATE'].includes(key) && isExcelDate(value)) {
+                            const excelEpoch = new Date(Date.UTC(1899, 11, 30)); // Excel starts at Dec 30, 1899
+                            value = moment.utc(excelEpoch.getTime() + value * 86400000).format('YYYY-MM-DD');
+                        }
 
                         // Convert 'USERNAME' to a string (even if it's a number)
                         if (['USERNAME', 'ID','FULL_NAME'].includes(key)) {
