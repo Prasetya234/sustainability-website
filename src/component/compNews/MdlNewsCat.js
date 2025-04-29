b22b48c0385d95d5f30174b9628453f145a23b2c
egiit <giefilt@gmail.com>
2025-04-29 21:50:41 +0700

    update update delete nerws category

--- src/component/compNews/MdlNewsCat.js
+++ src/component/compNews/MdlNewsCat.js
@@ -1,42 +1,9 @@
 import React from 'react'
-import axios from '../../axios/axios';
 import { Button, Col, Form, Modal, Row } from 'react-bootstrap'
-import { toast } from 'react-toastify';
 
-const MdlNewsCat = ({show, handleClose, newCatData, setNewCatData, userId, idPerusahaan, getNewsCategory}) => {
-    function chgData(e){
-        const {name, value} = e.target;
-        
-        if(name !== 'NEWS_CAT_ACTIVE'){
-        setNewCatData(prev => {
-            return {...prev, [name]: value};
-        });}else{            
-            setNewCatData(prev => {
-                return {...prev, [name]: value ? 1 : 0};
-            });
-        }
-    }
-    
-
-    async function submitNewCat(data) {
-        if(!data.NEWS_CAT_NAME) return;
-        const dataPost = {...data, ID_COMPANY : idPerusahaan, NEWS_CAT_ADD_ID : userId}
-        
-        await axios.post('/news/category', dataPost)
-        .then(res => {
-            if(res.status === 200){
-                toast.success(res.data.message, {autoClose : 2000})
-                getNewsCategory(idPerusahaan)
-                handleClose()
-            }
-            if(res.status === 202){
-                toast.error(res.data.message, {autoClose : 2000})
-            }
-        }).catch(err => {
-            console.log(err)
-            toast.error('Somthing whrong when add new categor y', {autoClose: 2000})
-        })
-    }
+const MdlNewsCat = ({show, handleClose, chgData, newCatData, submitNewCat}) => {
+   
+  
   return (
     <Modal show={show} onHide={handleClose}>
         <Modal.Header className='border-0' closeButton>
@@ -48,16 +15,16 @@ const MdlNewsCat = ({show, handleClose, newCatData, setNewCatData, userId, idPer
                     <Form>
                         <Form.Group className="mb-3" controlId="catName">
                             <Form.Label>Name</Form.Label>
-                            <Form.Control type="text" required name='NEWS_CAT_NAME'  value={newCatData.NEW_CAT_NAME}  onChange={chgData}/>
+                            <Form.Control type="text" required name='NEWS_CAT_NAME'  value={newCatData.NEWS_CAT_NAME}  onChange={chgData}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="catDesc">
                             <Form.Label>Description</Form.Label>
-                            <Form.Control as="textarea" rows={3}  name='NEWS_CAT_DESC' value={newCatData.NEW_CAT_DESC} onChange={chgData}/>
+                            <Form.Control as="textarea" rows={3}  name='NEWS_CAT_DESC' value={newCatData.NEWS_CAT_DESC} onChange={chgData}/>
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="activecat">
-                            <Form.Check value={newCatData.NEWS_CAT_ACTIVE} type="checkbox" label="Check for enable" name='NEWS_CAT_ACTIVE' onChange={chgData}/>
+                            <Form.Check checked={newCatData.NEWS_CAT_ACTIVE} type="checkbox" label="Check for enable" name='NEWS_CAT_ACTIVE' onChange={chgData}/>
                         </Form.Group>
                     </Form>
                 </Col>
--- src/pages/NewsCategory.js
+++ src/pages/NewsCategory.js
@@ -6,6 +6,7 @@ import { AuthContext } from '../auth/AuthProvider'
 import axios from '../axios/axios'
 import { toast } from 'react-toastify'
 import DropdownCus from '../partial/DropdownCus'
+import Swal from 'sweetalert2'
 
 const intialNewCat = {
     NEWS_CAT_NAME : '',
@@ -15,12 +16,13 @@ const intialNewCat = {
 
 
 const NewsCategory = () => {
-    const { value }                     = useContext(AuthContext);
-    const {  idPerusahaan, userId }     = value;
-    const [mdlCat, setMdlCat]           = useState(false)
-    const [newCatData, setNewCatData]   = useState(intialNewCat)
-    const [listCategory, setListCategory]   = useState([])
-    const [activeDropdown, setActiveDropdown] = useState(null);
+    const { value }                             = useContext(AuthContext);
+    const {  idPerusahaan, userId }             = value;
+    const [mdlCat, setMdlCat]                   = useState(false)
+    const [newCatData, setNewCatData]           = useState(intialNewCat)
+    const [listCategory, setListCategory]       = useState([])
+    const [activeDropdown, setActiveDropdown]   = useState(null);
+    const [type, setType]                       = useState('post');
 
     function activMdl(){
         setMdlCat(true)
@@ -46,15 +48,52 @@ const NewsCategory = () => {
         getNewsCategory(idPerusahaan)
     }, [idPerusahaan])
     
+    function handleOpnEdit(obj){
+        setNewCatData(obj)
+        setMdlCat(true)
+        setType('patch')
+    }
 
-    function actionList(id) {
+    async function deleteCatNews(objCat) {
+         Swal.fire({
+                 text: `Are You Delete ${objCat.NEWS_CAT_NAME} Category ?`,
+                 icon: "question",
+                 confirmButtonColor: "#2275f2",
+                 showCancelButton: true,
+                 confirmButtonText: "Yes",
+                 cancelButtonText: "Cancel",
+               }).then(async (result) => {
+                 if (result.isConfirmed) {
+                  const {NEWS_CAT_ID} = objCat
+        
+                   await axios
+                     .delete(`/news/category/${NEWS_CAT_ID}`)
+                     .then((res) => {
+                       if (res.status === 200) {
+                        getNewsCategory(idPerusahaan)
+                        toast.success(res.data.message, { autoClose: 2000 });
+                       }
+                       if(res.status === 202){
+                        toast.warning(res.data.message, { autoClose: 2000 });
+                       }
+                     })
+                     .catch((err) => {
+                       return toast.error("Somthing wrong when delete news category", {
+                         autoClose: 2500,
+                       });
+                     });
+                 }
+               });
+    }
+
+    function actionList(obj) {
         return [
         //   {
         //     actionLable: "Reset Password",
         //     actExe: () => openMdlResetPass(id),
         //   },
           { actionLable: "Edit", 
-            // actExe: () => editeBe(id)
+            actExe: () => handleOpnEdit(obj)
         },
         //   {
         //     actionLable: "Detail",
@@ -62,10 +101,46 @@ const NewsCategory = () => {
         //   },
           {
             actionLable: "Delete",
-            // actExe: () => deleteUser(id),
+            actExe: () => deleteCatNews(obj),
           },
         ];
       }
+
+
+        function chgData(e){
+              const {name, value} = e.target;
+              
+              if(name !== 'NEWS_CAT_ACTIVE'){
+              setNewCatData(prev => {
+                  return {...prev, [name]: value};
+              });
+            }else{            
+                  setNewCatData(prev => {
+                      return {...prev, [name]: value ? 1 : 0};
+                  });
+              }
+          }
+          
+      
+          async function submitNewCat(data) {        
+              if(!data.NEWS_CAT_NAME) return;
+              const dataPost = {...data, ID_COMPANY : idPerusahaan, NEWS_CAT_ADD_ID : userId}
+              
+              await axios[type]('/news/category', dataPost)
+              .then(res => {
+                  if(res.status === 200){
+                      toast.success(res.data.message, {autoClose : 2000})
+                      getNewsCategory(idPerusahaan)
+                      offMdl()
+                  }
+                  if(res.status === 202){
+                      toast.error(res.data.message, {autoClose : 2000})
+                  }
+              }).catch(err => {
+                  console.log(err)
+                  toast.error('Somthing whrong when add new categor y', {autoClose: 2000})
+              })
+          }
   return (
     <div>
         <Row className="m-0 mt-2">
@@ -99,7 +174,7 @@ const NewsCategory = () => {
                                             <DropdownCus
                                             label={"Action"}
                                             dropdownId={`dropdown${item.NEWS_CAT_ID}`}
-                                            items={actionList(item.USER_ID)}
+                                            items={actionList(item)}
                                             activeDropdown={activeDropdown}
                                             setActiveDropdown={setActiveDropdown}
                                             />
@@ -117,10 +192,8 @@ const NewsCategory = () => {
         <MdlNewsCat show={mdlCat} 
         handleClose={offMdl} 
         newCatData={newCatData} 
-        setNewCatData={setNewCatData} 
-        idPerusahaan={idPerusahaan}
-        userId={userId}
-        getNewsCategory={getNewsCategory}
+        chgData={chgData}
+        submitNewCat={submitNewCat}
         />
     </div>
   )
